69434308941a4a0f09407f24a1a0edcea4fcd284
Sharifa <shar.samuels@gmail.com>
2016-11-14 16:01:04 -0800

    list-test forms in Angular on first day

--- src/app/list-test/list-test.component.ts
+++ src/app/list-test/list-test.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 // define data by name
-interface Apple {
-  type: string;
-  colour: string;
+class Apple {
+  constructor(
+    public name: string,
+    public colour: string
+) {}
 
 }
+
+
 @Component({
   selector: 'app-list-test',
   templateUrl: './list-test.component.html',
   styleUrls: ['./list-test.component.css']
 })
 export class ListTestComponent implements OnInit {
-  appleList: [Apple];
+
+  appleList: [Apple]; //list
+  apple: Apple; //form
     
   constructor() { 
+    this.apple = {name: '', colour: ''};
   this.appleList = [
-  {type: 'Granny Smith', colour: 'green' },  
-  {type: 'Delicious', colour: 'Deep Red'},
-  {type: 'Crab', colour: 'Crab'}
+  {name: 'Granny Smith', colour: 'green' },
+  {name: 'Delicious', colour: 'Deep Red'},
+  {name: 'Crab', colour: 'Crab'}
   ];
 }
 addApple() {
-  const apple = { type: 'Macintosh', colour: 'Red'};
-  this.appleList.push(apple);
+this.appleList.push(this.apple);
+this.apple = {name: '', colour: ''};
+
 }
-
-
-
+// renderAppleForm(appleForm){
+//   console.log(appleForm);
+// }
 
   ngOnInit() {
   }
